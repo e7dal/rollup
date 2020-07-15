7bf9aab14b746c1546c14987e9b7eea2f6afd775
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2020-07-15 07:02:49 +0200

    Generate proper interop namespaces when losing track of a namespace, freeze interop namespaces, mark interop helpers as pure

--- test/chunking-form/samples/preserve-modules-commonjs/_expected/amd/commonjs.js
+++ test/chunking-form/samples/preserve-modules-commonjs/_expected/amd/commonjs.js
@@ -2,7 +2,7 @@ define(['exports', 'external', './other'], function (exports, external, other) {
 
 	function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex : { 'default': ex }; }
 
-	var external__default = _interopDefault(external);
+	var external__default = /*#__PURE__*/_interopDefault(external);
 
 	const { value } = other.default;
 
